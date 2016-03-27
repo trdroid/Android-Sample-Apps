aa07b420b1fd8e01515103fb9f31d863f7e1c0cf
gruprog <gruprog@gmail.com>
2016-03-27 09:29:57 -0400

    code changes

--- WeatherApp/app/src/main/java/com/gruprog/weatherapp/MainActivity.java
+++ WeatherApp/app/src/main/java/com/gruprog/weatherapp/MainActivity.java
@@ -15,17 +15,15 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
+        if(savedInstanceState == null) {
+            /*
+                add an instance of the MainFragment at R.id.root_container
+             */
+            getSupportFragmentManager().beginTransaction()
+                    .add(R.id.root_container, new MainFragment())
+                    .commit();
+        }
     }
 
     @Override
---
+++ WeatherApp/app/src/main/java/com/gruprog/weatherapp/MainFragment.java
@@ -0,0 +1,48 @@
+package com.gruprog.weatherapp;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class MainFragment extends Fragment {
+    ArrayAdapter<String> weatherDataAdapter;
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        String[] weatherData = {
+                "Monday",
+                "Tuesday",
+                "Wednesday",
+                "Thursday",
+                "Friday",
+                "Saturday",
+                "Sunday"
+        };
+
+        List<String> weatherDataList = new ArrayList<>(Arrays.asList(weatherData));
+
+        weatherDataAdapter = new ArrayAdapter<>(
+                this.getActivity(),
+                R.layout.day_weather_forecast_list_item_layout, /* Point to the resource ID of the layout that contains the view for each item in the list */
+                R.id.day_weather_forecast_list_item_text_view, /* Point to the resource ID of the TextView in the above specified layout.
+                 The Adapter instantiates a TextView per data element to display a kist of TextViews/Data elements in the ListView. */
+                weatherDataList); 
+
+        View rootView = inflater.inflate(R.layout.fragment_main, container, false);
+
+        ListView listView = (ListView) rootView.findViewById(R.id.weather_forecast_list_view);
+        listView.setAdapter(weatherDataAdapter);
+
+        return rootView;
+    }
+}
