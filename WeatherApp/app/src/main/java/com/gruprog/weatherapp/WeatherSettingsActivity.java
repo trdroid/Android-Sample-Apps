4579c8d7235702c1b4fe1eefff0584dcc7af99db
Jai Srikar <gruprog@gmail.com>
2016-09-18 23:43:38 -0400

    a simple space shooter game app

---
+++ WeatherApp/app/src/main/java/com/gruprog/weatherapp/WeatherSettingsActivity.java
@@ -0,0 +1,28 @@
+package com.gruprog.weatherapp;
+
+import android.app.Activity;
+import android.preference.PreferenceFragment;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class WeatherSettingsActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_weather_settings);
+    }
+
+    public static class WeatherSettingsFragment extends PreferenceFragment {
+        @Override
+        public void onCreate(@Nullable Bundle savedInstanceState) {
+            super.onCreate(savedInstanceState);
+            addPreferencesFromResource();
+        }
+    }
+}
